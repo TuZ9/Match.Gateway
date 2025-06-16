a092b2c01a3a82f3d3f7dde027e5c64a1f475e1e
Arthur Vaz <arthur.vaz95@btgpactual.com>
2025-06-15 23:05:31 -0300

    build

--- Suitability.Gateway.Infrastructure/Extensions/HttpClient.cs
+++ Suitability.Gateway.Infrastructure/Extensions/HttpClient.cs
@@ -15,15 +15,15 @@ namespace Suitability.Gateway.Infrastructure.Extensions
             var accountLogger = serviceProvider.GetService(typeof(ILogger<AccountApiClient>)) as ILogger<AccountApiClient>;
             var strainLogger = serviceProvider.GetService(typeof(ILogger<DocumentStatusApiClient>)) as ILogger<DocumentStatusApiClient>;
 
-            services.AddHttpClient("Account",
-                client => { client.BaseAddress = new Uri(RunTimeConfig.SuitabilityEndpoint); });
-            services.AddHttpClient("DocumentStatus",
-                client => { client.BaseAddress = new Uri(RunTimeConfig.SuitabilityEndpoint); });
-            services.AddHttpClient<IAccountApiClient, AccountApiClient>(_ => _.BaseAddress = new Uri(RunTimeConfig.SuitabilityEndpoint));
-            services.AddHttpClient<IDocumentStatusApiClient, DocumentStatusApiClient>(_ => _.BaseAddress = new Uri(RunTimeConfig.SuitabilityEndpoint));
+            services.AddHttpClient("Account", client => { client.BaseAddress = new Uri(RunTimeConfig.SuitabilityEndpoint); });
+            services.AddHttpClient("DocumentStatus", client => { client.BaseAddress = new Uri(RunTimeConfig.SuitabilityEndpoint); });
+
             services.AddSingleton<IAccountApiClient, AccountApiClient>(x => new AccountApiClient(x.GetService<IHttpClientFactory>()!, accountLogger, "Account"));
             services.AddSingleton<IDocumentStatusApiClient, DocumentStatusApiClient>(x => new DocumentStatusApiClient(x.GetService<IHttpClientFactory>()!, strainLogger, "DocumentStatus"));
-            
+
+            //services.AddHttpClient<IAccountApiClient, AccountApiClient>(_ => _.BaseAddress = new Uri(RunTimeConfig.SuitabilityEndpoint));
+            //services.AddHttpClient<IDocumentStatusApiClient, DocumentStatusApiClient>(_ => _.BaseAddress = new Uri(RunTimeConfig.SuitabilityEndpoint));
+
             return services;
         }
     }
