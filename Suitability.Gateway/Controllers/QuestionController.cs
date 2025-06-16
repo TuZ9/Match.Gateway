327a7c36faea08c1fa47f925e5942561b5a1d73d
Arthur Vaz <arthur.vaz95@btgpactual.com>
2025-06-16 01:08:09 -0300

    Add Test

---
+++ Suitability.Gateway/Controllers/QuestionController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Suitability.Gateway.Domain.Interfaces.Services;
+
+namespace Suitability.Gateway.Api.Controllers
+{
+    [ApiController]
+    [EnableCors("All")]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IGatewayService _gatService;
+        public QuestionController(IGatewayService gatService)
+        {
+            _gatService = gatService;
+        }
+
+        [Route("health")]
+        [HttpGet]
+        public async Task<IActionResult> GetHealth()
+        {
+            return Ok(new[] { "Gateway Service Health Sucess" });
+        }
+
+        [Route("ConsumeProducts")]
+        [HttpGet]
+        public async Task<IActionResult> Gettest()
+        {
+            await _gatService.ConsumeProducts();
+
+            return Ok(new[] { "Gateway Service Health Sucess" });
+        }
+    }
+}
